736b863ab2a9dc25a80e40cefa146bc259cf1303
nicopatoco <nicolasherrera.cp@gmail.com>
2024-03-11 15:14:29 +0100

    feat: config cypress for e2e test

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,13 @@ import { defineConfig } from 'cypress';
 import vitePreprocessor from 'cypress-vite';
 
 export default defineConfig({
+  component: {
+    devServer: {
+      framework: 'react',
+      bundler: 'vite',
+    },
+  },
+
   e2e: {
     setupNodeEvents(on) {
       on('file:preprocessor', vitePreprocessor());
