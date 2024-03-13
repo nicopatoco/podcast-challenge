d2e8ff85230e0173ab461a8a35af455de18cc847
nicopatoco <nicolasherrera.cp@gmail.com>
2024-03-13 11:36:05 +0100

    feat: prevent html inside description

--- src/components/EpisodeDetails.tsx
+++ src/components/EpisodeDetails.tsx
@@ -1,16 +1,22 @@
 import { PodcastEpisode } from '../types/episode';
 import AudioPlayer from './AudioPlayer';
 
-type episodeProps = {
+type EpisodeProps = {
   episode: PodcastEpisode;
 };
 
-export default function EpisodeDetails({ episode }: episodeProps) {
+export default function EpisodeDetails({ episode }: EpisodeProps) {
+  // Function to create markup
+  const createMarkup = (htmlContent: string) => {
+    return { __html: htmlContent };
+  };
+
   return (
     <div className="flex flex-col">
       <div className="p-2 mb-2 bg-white border border-gray-200 rounded-lg shadow-md max-w-88 dark:bg-gray-800 dark:border-gray-700">
         <div className="mb-2 font-bold text-xxl">{episode.trackName}</div>
-        <p className="mb-8 text-xs">{episode.description}</p>
+        {/* Use dangerouslySetInnerHTML to render HTML */}
+        <p className="mb-8 text-xs" dangerouslySetInnerHTML={createMarkup(episode.description)}></p>
         <AudioPlayer src={episode.episodeUrl} />
       </div>
     </div>
